d8f045355308bd9b4c456600b94de8637cb29e9e
Dav&Iv <dav.ivediting@gmail.com>
2026-03-25 01:53:55 -0400

    We implement the main for the second exercice

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import oop.exercice1.Student;
+import oop.exercice2.MathUtils;
+
 import java.util.Scanner;
 
 void main() {
@@ -20,6 +22,63 @@ void main() {
     Student student = new Student(name, age, averageGrade);
 
     System.out.println(student);
+
+    // We create a new object called math --> we are realizing here a sum, which is a math operation (the name doesn't really matter, it just to be coherent)
+    MathUtils math = new MathUtils();
+
+    // We allow the user to choose which method he wants to try
+    System.out.println("Choose type:");
+    System.out.println("1 = int");
+    System.out.println("2 = double");
+    System.out.println("3 = String");
+    System.out.println("4 = three ints");
+    System.out.print("Your choice: ");
+    int choice = scanner.nextInt();
+
+    // We initialize the different parameters
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    double da = 0.0;
+    double db = 0.0;
+    String sa = "";
+    String sb = "";
+
+    // We use a if, else statement so it can apply each method propreties
+    if (choice == 1) {
+        System.out.print("Enter first int: ");
+        a = scanner.nextInt();
+        System.out.print("Enter second int: ");
+        b = scanner.nextInt();
+        System.out.println("Result: " + math.sum(a, b));
+
+    } else if (choice == 2) {
+        System.out.print("Enter first double: ");
+        da = scanner.nextDouble();
+        System.out.print("Enter second double: ");
+        db = scanner.nextDouble();
+        System.out.println("Result: " + math.sum(da, db));
+
+    } else if (choice == 3) {
+        System.out.print("Enter first String: ");
+        sa = scanner.nextLine();
+        System.out.print("Enter second String: ");
+        sb = scanner.nextLine();
+        System.out.println("Result: " + math.sum(sa, sb));
+
+    } else if (choice == 4) {
+        System.out.print("Enter first int: ");
+        a = scanner.nextInt();
+        System.out.print("Enter second int: ");
+        b = scanner.nextInt();
+        System.out.print("Enter third int: ");
+        c = scanner.nextInt();
+        System.out.println("Result: " + math.sum(a, b, c));
+
+    } else {
+        System.out.println("Invalid choice.");
+    }
+
 
     scanner.close();
 }
