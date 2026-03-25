c1dc3de00cde1b229e7be4af3b012f1b896c20e5
Dav&Iv <dav.ivediting@gmail.com>
2026-03-25 01:41:37 -0400

    We implemented the method of the MathUtils, which are overloaded methods

---
+++ src/oop/exercice2/MathUtils.java
@@ -0,0 +1,26 @@
+package oop.exercice2;
+
+public class MathUtils {
+
+    // Two integers
+    public int sum(int a, int b){
+        return a+ b;
+    }
+
+    // Two double
+    public double sum(double a, double b){
+        return a+ b;
+    }
+
+    // Two String - concatenation
+    public String sum(String a, String b){
+        return a+ b;
+    }
+
+    // Three integers
+    public int sum(int a, int b, int c){
+        return a+ b;
+    }
+
+
+}
