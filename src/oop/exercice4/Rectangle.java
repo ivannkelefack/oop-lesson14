96c86f7d0e9a030d91ac1215a244e61729883472
Dav&Iv <dav.ivediting@gmail.com>
2026-03-27 13:16:51 -0400

    Added exercise 4 which introduces the Drawable interface with two methods draw() and calculateArea(). Implemented the interface in Circle and Rectangle classes, each with their own fields, constructor and formulas for the area. In the main, the user can create several shapes stored in a List<Drawable> and the program loops through to call draw() and calculateArea() on each one, demonstrating polymorphism through interfaces.

---
+++ src/oop/exercice4/Rectangle.java
@@ -0,0 +1,22 @@
+package oop.exercice4;
+
+public class Rectangle implements Drawable {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle with width " + width + " and height " + height);
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
